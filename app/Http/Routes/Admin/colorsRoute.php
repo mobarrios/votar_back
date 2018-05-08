185aea038679706088d520cb225e89d85ef0e9aa
Manuel Barrios <mbarrios@Manuels-MacBook-Pro.local>
2018-05-08 16:30:26 -0300

    fix routes colors

--- app/Http/Routes/Admin/colorsRoute.php
+++ app/Http/Routes/Admin/colorsRoute.php
@@ -4,14 +4,14 @@ Route::group(['prefix'=>'colors'],function(){
 
         $section =  'colors';
 
-        Route::get('/destroy/{id?}',    ['middleware'=>'permission:'.$section.'.destroy','as'=>'admin.colors.destroy','uses'=>'admin\ColorsController@destroy']);
-        Route::get('/edit/{id?}',       ['middleware'=>'permission:'.$section.'.edit','as'=>'admin.colors.edit','uses'=>'admin\ColorsController@edit']);
-        Route::post('/update/{id?}',    ['middleware'=>'permission:'.$section.'.edit','as'=>'admin.colors.update','uses'=>'admin\ColorsController@update']);
+        Route::get('/destroy/{id?}',    ['middleware'=>'permission:'.$section.'.destroy','as'=>'admin.colors.destroy','uses'=>'Admin\ColorsController@destroy']);
+        Route::get('/edit/{id?}',       ['middleware'=>'permission:'.$section.'.edit','as'=>'admin.colors.edit','uses'=>'Admin\ColorsController@edit']);
+        Route::post('/update/{id?}',    ['middleware'=>'permission:'.$section.'.edit','as'=>'admin.colors.update','uses'=>'Admin\ColorsController@update']);
 
-        Route::get('/create',           ['middleware'=>'permission:'.$section.'.create','as'=>'admin.colors.create','uses'=>'admin\ColorsController@create']);
-        Route::post('/store',           ['middleware'=>'permission:'.$section.'.create','as'=>'admin.colors.store','uses'=>'admin\ColorsController@store']);
-        Route::get('/show',             ['middleware'=>'permission:'.$section.'.show','as'=>'admin.colors.show','uses'=>'admin\ColorsController@show']);
-        Route::get('/index/{search?}',  ['middleware'=>'permission:'.$section.'.list','as'=>'admin.colors.index','uses'=>'admin\ColorsController@index']);
+        Route::get('/create',           ['middleware'=>'permission:'.$section.'.create','as'=>'admin.colors.create','uses'=>'Admin\ColorsController@create']);
+        Route::post('/store',           ['middleware'=>'permission:'.$section.'.create','as'=>'admin.colors.store','uses'=>'Admin\ColorsController@store']);
+        Route::get('/show',             ['middleware'=>'permission:'.$section.'.show','as'=>'admin.colors.show','uses'=>'Admin\ColorsController@show']);
+        Route::get('/index/{search?}',  ['middleware'=>'permission:'.$section.'.list','as'=>'admin.colors.index','uses'=>'Admin\ColorsController@index']);
 
     Route::get('/pdf',  ['middleware'=>'permission:'.$section.'.list','as'=>'admin.colors.pdf','uses'=>'Utilities\UtilitiesController@exportListToPdf']);
 
