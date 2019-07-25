c7b82b62387bbd2820823cab4bca4878c3622e71
Manuel Barrios <mbarrios@Manuels-MBP.utopia.net>
2019-07-25 09:29:11 -0300

    repo

--- resources/views/admin/operativos/show.blade copy.php
+++ resources/views/admin/operativos/show.blade copy.php
@@ -2,7 +2,7 @@
 
     @section('sectionContent')
 
-        {{-- <div class="row">
+        <div class="row">
             
             <div class="col-xs-12">
 
@@ -11,100 +11,27 @@
                         <h1>{{$models->nombre}}</h1>
                     </div>
                     <div class="box-body">
-                        <div class="row"> --}}
+                        <div class="row">
 
-            <div class="box">
-                <div class="box-header">
-                    <h1 class="text-center">Presidente - Vice</h1>
-                </div>
-                <div class="box-body">
-                    @foreach($models->Listas->sortBy('tipo_operativos_id') as $lista)
-                        @if($lista->tipo_operativos_id == 1)
-                            <div class="col-xs-1 ">
-                                <img width="100px" src="{{$lista->Partidos->Images->first()->path or ''}}">
-                            </div>
-                            <div class="col-xs-8">
-                                <h2>{{$lista->nombre}}</h2>
-                                <h3>{{$lista->TipoOperativos->nombre}}</h3>
-                            </div>
-                            <div class="col-xs-3">
-                                @if($total > 0)
-                                {{--   <h1><strong class="text-danger">{{ number_format((($lista->VotosOperativos($models->id)->sum('total')*100)/ $total),2) }} %</strong></h1> --}}
-                                    <h1><strong class="text-danger">{{ number_format(( (($lista->VotosOperativos($models->id)->sum('total'))*100 ) / $lista->VotosTipoOperativos($models->id,$lista->tipo_operativos_id)->total),2) }} %</strong></h1>
-                                @else
-                                    <h1><strong>0</strong>%</h1>
-                                @endif
-                                <h3>{{$lista->VotosOperativos($models->id)->sum('total')}} votos</h3>
-                            </div> 
-                        @endif
-                    @endforeach
-                    <canvas id="t1"></canvas>
-                </div>
-            </div>
+                                 @foreach($models->Listas->sortBy('tipo_operativos_id') as $lista)
 
-
-            <div class="box">
-                    <div class="box-header">
-                        <h1 class="text-center">Gobernadores</h1>
-                    </div>
-                    <div class="box-body">
-                        @foreach($models->Listas->sortBy('tipo_operativos_id') as $lista)
-                            @if($lista->tipo_operativos_id == 2)
-                                <div class="col-xs-1 ">
-                                    <img width="100px" src="{{$lista->Partidos->Images->first()->path or ''}}">
-                                </div>
-                                <div class="col-xs-8">
-                                    <h2>{{$lista->nombre}}</h2>
-                                    <h3>{{$lista->TipoOperativos->nombre}}</h3>
-                                </div>
-                                <div class="col-xs-3">
-                                    @if($total > 0)
-                                    {{--   <h1><strong class="text-danger">{{ number_format((($lista->VotosOperativos($models->id)->sum('total')*100)/ $total),2) }} %</strong></h1> --}}
-                                        <h1><strong class="text-danger">{{ number_format(( (($lista->VotosOperativos($models->id)->sum('total'))*100 ) / $lista->VotosTipoOperativos($models->id,$lista->tipo_operativos_id)->total),2) }} %</strong></h1>
-                                    @else
-                                        <h1><strong>0</strong>%</h1>
-                                    @endif
-                                    <h3>{{$lista->VotosOperativos($models->id)->sum('total')}} votos</h3>
-                                </div> 
-                            @endif
-                        @endforeach
-                        <canvas id="t2"></canvas>
-                    </div>
-                </div>
-
-
-
-                <div class="box">
-                        <div class="box-header">
-                            <h1 class="text-center">Legisladores</h1>
-                        </div>
-                        <div class="box-body">
-                            @foreach($models->Listas->sortBy('tipo_operativos_id') as $lista)
-                                @if($lista->tipo_operativos_id == 3)
-                                    <div class="col-xs-1 ">
+                                     <div class="col-xs-1 ">
                                         <img width="100px" src="{{$lista->Partidos->Images->first()->path or ''}}">
                                     </div>
                                     <div class="col-xs-8">
-                                        <h2>{{$lista->nombre}}</h2>
-                                        <h3>{{$lista->TipoOperativos->nombre}}</h3>
+                                            <h2>{{$lista->nombre}}</h2>
+                                            <h3>{{$lista->TipoOperativos->nombre}}</h3>
                                     </div>
                                     <div class="col-xs-3">
-                                        @if($total > 0)
-                                        {{--   <h1><strong class="text-danger">{{ number_format((($lista->VotosOperativos($models->id)->sum('total')*100)/ $total),2) }} %</strong></h1> --}}
-                                            <h1><strong class="text-danger">{{ number_format(( (($lista->VotosOperativos($models->id)->sum('total'))*100 ) / $lista->VotosTipoOperativos($models->id,$lista->tipo_operativos_id)->total),2) }} %</strong></h1>
-                                        @else
-                                            <h1><strong>0</strong>%</h1>
-                                        @endif
-                                        <h3>{{$lista->VotosOperativos($models->id)->sum('total')}} votos</h3>
-                                    </div> 
-                                @endif
-                            @endforeach
-                            <canvas id="t3"></canvas>
-                        </div>
-                    </div>
-
-                    <div class="box">
-                            <div class="box-body"> 
+                                            @if($total > 0)
+                                              {{--   <h1><strong class="text-danger">{{ number_format((($lista->VotosOperativos($models->id)->sum('total')*100)/ $total),2) }} %</strong></h1> --}}
+                                                <h1><strong class="text-danger">{{ number_format(( (($lista->VotosOperativos($models->id)->sum('total'))*100 ) / $lista->VotosTipoOperativos($models->id,$lista->tipo_operativos_id)->total),2) }} %</strong></h1>
+                                            @else
+                                                <h1><strong>0</strong>%</h1>
+                                            @endif
+                                            <h3>{{$lista->VotosOperativos($models->id)->sum('total')}} votos</h3>
+                                    </div>                                       
+                                @endforeach
                                 <div class="col-xs-1 ">       
                                 </div>
                                 <div class="col-xs-8 ">
@@ -137,37 +64,36 @@
                                 <div class="col-xs-3">
                                       <h1 class="text-danger" ><strong>{{$models->Votos->sum('total_recurridos') }}</strong></h1>
                                 </div>
-                            </div>
-                    </div>
-                        {{-- </div>
-                    </div>
-                </div> --}}
 
-                {{-- <div class="box">  
+                        </div>
+                    </div>
+                </div>
+
+                <div class="box">  
                         <div class="box-body">
                              <div class="box-title">
                                 <h3>Presidente - Vice</h3>
                              </div>
                             <canvas id="t1" ></canvas>
                         </div>
-                    </div> --}}
+                    </div>
 
-                {{-- <div class="box">  
+                <div class="box">  
                     <div class="box-body">
                          <div class="box-title">
                             <h3>Gobernadores</h3>
                          </div>
                         <canvas id="t2" ></canvas>
                     </div>
-                </div> --}}
-                  {{-- <div class="box">  
+                </div>
+                  <div class="box">  
                     <div class="box-body">
                          <div class="box-title">
                             <h3>Legisladores</h3>
                          </div>
                         <canvas id="t3" ></canvas>
                     </div>
-                </div> --}}
+                </div>
                  {{-- <div class="box">  
                     <div class="box-body">
                          <div class="box-title">
@@ -199,13 +125,15 @@
                                         <td>{{$municipio->total}}</td>
                                     </tr> 
                                     @endforeach
+                                    
                                 </tbody>
                             </table>
+                            
                         </div>
                     </div>
-            {{-- </div>
+            </div>
 
-        </div> --}}
+        </div>
 
 
 
